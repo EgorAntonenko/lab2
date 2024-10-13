8ce86b71109867e8d570387164bae009bb032c23
EgorAntonenko <155195428+EgorAntonenko@users.noreply.github.com>
2024-10-13 18:35:36 +0300

    Add files via upload
    Adding my project files to my repository

---
+++ lab2/complete/src/test/java/SimpleTest.java
@@ -0,0 +1,17 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+
+    @Test
+    public void testPassing() {
+        // Цей тест пройде успішно
+        assertEquals(2, 1 + 1);
+    }
+
+//    @Test
+//    public void testFailing() {
+//        // Цей тест не пройде
+//        assertEquals(2, 5 - 1);  // Цей тест не пройде, бо 5 - 1 дорівнює 4
+//    }
+}
